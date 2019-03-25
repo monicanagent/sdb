10284455e6f86040ad65c47289431a4887f9696c
Patrick Bay <monicanagent@users.noreply.github.com>
2019-03-25 12:58:49 -0400

    Added a few more console.log lines to make the output more readable.

--- example/cryptoSDB.js
+++ example/cryptoSDB.js
@@ -30,23 +30,36 @@ var JSONSDB = [
 ];
 
 async function example() {
+   console.log ("Native JSON SDB Data");
+   console.log ("----------------------");
+   console.log ("");
+   console.dir (JSONSDB);
+   console.log ("");
    var JSONSDBString = JSON.stringify(JSONSDB, 0);
    var encryptSDB = new SDB();
    var result = encryptSDB.decode(JSONSDB); //just assigns it since it's an array
    var plainBase85SDB = await encryptSDB.encode("base85"); //convert to plaintext Base58
    // Sender encrypts the SDB...
+   console.log ("-------------------------");
    console.log ("Plaintext Base85 SDB Data");
    console.log ("-------------------------");
+   console.log ("");
    console.log (plainBase85SDB);
+   console.log ("");
    var base85SDB = await encryptSDB.encode("base85", encodePipe); //convert to Base58 with encoding (encryption) pipe
+   console.log ("-------------------------");
    console.log ("Encrypted Base85 SDB Data");
    console.log ("-------------------------");
+   console.log ("");
    console.log (base85SDB);
+   console.log ("");
    // ... then sends base85SDB to receiving peer who can then...
    var decryptSDB = new SDB();
    var result = await decryptSDB.decode(base85SDB, "base85", decodePipe); //convert from Base58 with decoding (decryption) pipe
+   console.log ("-------------------------");
    console.log ("Decrypted Base85 SDB Data");
    console.log ("-------------------------");
+   console.log ("");
    console.dir (decryptSDB.data);
    // ... use decryptSDB.data to determine connectivity options.
 }
