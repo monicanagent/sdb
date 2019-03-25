be9a064e656198245c1aa8a3c70f44f5419d7130
Patrick Bay <monicanagent@users.noreply.github.com>
2019-03-25 11:55:49 -0400

    Updated examples to use new asynchronous functionality.

--- example/base85.js
+++ example/base85.js
@@ -4,7 +4,7 @@
 */
 const SDB = require("../");
 
-var nativeSDB = [
+var JSONSDB = [
    {
       "entity":"api",
       "name": "CypherPoker.JS Services",
@@ -23,32 +23,35 @@ var nativeSDB = [
    }
 ];
 
-var nativeSDBString = JSON.stringify(nativeSDB, 0);
-var nativeSize = nativeSDBString.length;
-console.log ("Native (JSON) Minified SDB Data");
-console.log ("--------------------------------");
-console.log (nativeSDBString);
-console.log ("");
-console.log(" ");
-var testSDB = new SDB(nativeSDB);
-var base85SDB = testSDB.encode("base85");
-var base85SDBs = base85SDB.substring(2, base85SDB.length);
-base85SDBs = base85SDBs.substring(0, (base85SDBs.length-2));
-var base85SDBSize = base85SDB.length;
-var base85SDBsSize = base85SDBs.length;
-console.log ("Base85 SDB Data");
-console.log ("---------------");
-console.log (base85SDB);
-console.log ("");
-console.log ("Base85 SDB-s (short) Data");
-console.log ("-------------------------");
-console.log (base85SDBs);
-console.log ("");
-console.log ("Data Size (SDB-JSON-m): "+nativeSize+" bytes");
-console.log ("Data Size (SDB-b85): "+base85SDBSize+" bytes");
-console.log ("Data Size (SDB-b85-s): "+base85SDBsSize+" bytes");
+async function example() {
+   var JSONSDBString = JSON.stringify(JSONSDB, 0);
+   var nativeSize = JSONSDBString.length;
+   console.log ("Native (JSON) Minified SDB Data");
+   console.log ("--------------------------------");
+   console.log (JSONSDBString);
+   console.log ("");
+   console.log(" ");
+   var testSDB = new SDB();
+   var result = await testSDB.decode(JSONSDB); //just assigns it since it's an array
+   var base85SDB = await testSDB.encode("base85");
+   var base85SDBs = base85SDB.substring(2, base85SDB.length);
+   base85SDBs = base85SDBs.substring(0, (base85SDBs.length-2));
+   var base85SDBSize = base85SDB.length;
+   var base85SDBsSize = base85SDBs.length;
+   console.log ("Base85 SDB Data");
+   console.log ("---------------");
+   console.log (base85SDB);
+   console.log ("");
+   console.log ("Base85 SDB-s (short) Data");
+   console.log ("-------------------------");
+   console.log (base85SDBs);
+   console.log ("");
+   console.log ("Data Size (SDB-JSON-m): "+nativeSize+" bytes");
+   console.log ("Data Size (SDB-b85): "+base85SDBSize+" bytes");
+   console.log ("Data Size (SDB-b85-s): "+base85SDBsSize+" bytes");
+   console.log(" ");
+   console.log ("Base58 SDB is approximately "+(100 - Math.floor((base85SDBSize / nativeSize) * 100))+"% smaller than JSON-m SDB data.");
+   console.log ("Base58 SDB-s is approximately "+(100 - Math.floor((base85SDBsSize / nativeSize) * 100))+"% smaller than JSON-m SDB data.");
+}
 
-console.log(" ");
-
-console.log ("Base58 SDB is approximately "+(100 - Math.floor((base85SDBSize / nativeSize) * 100))+"% smaller than JSON-m SDB data.");
-console.log ("Base58 SDB-s is approximately "+(100 - Math.floor((base85SDBsSize / nativeSize) * 100))+"% smaller than JSON-m SDB data.");
+example();
--- example/tinySDB.js
+++ example/tinySDB.js
@@ -5,47 +5,50 @@
 */
 const SDB = require("../");
 
-var nativeSDB = [
+var JSONSDB = [
    {
       "entity":"api",
       "url":"http://localhost:8090"
    }
 ];
 
-var nativeSDBString = JSON.stringify(nativeSDB, 0);
-var nativeSize = nativeSDBString.length;
-console.log ("Native (JSON) Minified SDB Data");
-console.log ("--------------------------------");
-console.log (nativeSDBString);
-console.log ("");
-console.log(" ");
-var testSDB = new SDB(nativeSDB);
-var base85SDB = testSDB.encode("base85");
-var base85SDBs = base85SDB.substring(2, base85SDB.length);
-var base64SDB = testSDB.encode("base64");
-base85SDBs = base85SDBs.substring(0, (base85SDBs.length-2));
-var base85SDBSize = base85SDB.length;
-var base85SDBsSize = base85SDBs.length;
-var base64SDBSize = base64SDB.length;
-console.log ("Base85 SDB Data");
-console.log ("---------------");
-console.log (base85SDB);
-console.log ("");
-console.log ("Base85 SDB-s (short) Data");
-console.log ("-------------------------");
-console.log (base85SDBs);
-console.log ("");
-console.log ("Base64 SDB Data");
-console.log ("---------------");
-console.log (base64SDB);
-console.log ("");
-console.log ("Data Size (SDB-JSON-m): "+nativeSize+" bytes");
-console.log ("Data Size (SDB-b85): "+base85SDBSize+" bytes");
-console.log ("Data Size (SDB-b85-s): "+base85SDBsSize+" bytes");
-console.log ("Data Size (SDB-b64): "+base64SDBSize+" bytes");
+async function example() {
+   var JSONSDBString = JSON.stringify(JSONSDB, 0);
+   var nativeSize = JSONSDBString.length;
+   console.log ("Native (JSON) Minified SDB Data");
+   console.log ("--------------------------------");
+   console.log (JSONSDBString);
+   console.log ("");
+   console.log(" ");
+   var testSDB = new SDB();
+   var result = await testSDB.decode(JSONSDB); //just assigns it since it's an array
+   var base85SDB = await testSDB.encode("base85");
+   var base85SDBs = base85SDB.substring(2, base85SDB.length);
+   var base64SDB = await testSDB.encode("base64");
+   base85SDBs = base85SDBs.substring(0, (base85SDBs.length-2));
+   var base85SDBSize = base85SDB.length;
+   var base85SDBsSize = base85SDBs.length;
+   var base64SDBSize = base64SDB.length;
+   console.log ("Base85 SDB Data");
+   console.log ("---------------");
+   console.log (base85SDB);
+   console.log ("");
+   console.log ("Base85 SDB-s (short) Data");
+   console.log ("-------------------------");
+   console.log (base85SDBs);
+   console.log ("");
+   console.log ("Base64 SDB Data");
+   console.log ("---------------");
+   console.log (base64SDB);
+   console.log ("");
+   console.log ("Data Size (SDB-JSON-m): "+nativeSize+" bytes");
+   console.log ("Data Size (SDB-b85): "+base85SDBSize+" bytes");
+   console.log ("Data Size (SDB-b85-s): "+base85SDBsSize+" bytes");
+   console.log ("Data Size (SDB-b64): "+base64SDBSize+" bytes");
+   console.log(" ");
+   console.log ("Base58 SDB is approximately "+(100 - Math.floor((base85SDBSize / nativeSize) * 100))+"% smaller than JSON-m SDB data.");
+   console.log ("Base58 SDB-s is approximately "+(100 - Math.floor((base85SDBsSize / nativeSize) * 100))+"% smaller than JSON-m SDB data.");
+   console.log ("Base64 SDB is approximately "+(100 - Math.floor((base64SDBSize / nativeSize) * 100))+"% smaller than JSON-m SDB data.");
+}
 
-console.log(" ");
-
-console.log ("Base58 SDB is approximately "+(100 - Math.floor((base85SDBSize / nativeSize) * 100))+"% smaller than JSON-m SDB data.");
-console.log ("Base58 SDB-s is approximately "+(100 - Math.floor((base85SDBsSize / nativeSize) * 100))+"% smaller than JSON-m SDB data.");
-console.log ("Base64 SDB is approximately "+(100 - Math.floor((base64SDBSize / nativeSize) * 100))+"% smaller than JSON-m SDB data.");
+example();
